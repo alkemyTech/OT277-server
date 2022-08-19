8a9dce12a9f6e7e7989933b6b7215e92aacb9381
Cristian Monzon <cristianmonzon91@gmail.com>
2022-08-19 20:47:09 -0300

    modelo y repositorio base de testimonials

---
+++ src/main/java/com/alkemy/ong/entity/TestimonialEntity.java
@@ -0,0 +1,46 @@
+package com.alkemy.ong.entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE testimonials SET delete = true WHERE id = ?")
+@Where(clause = "delete = false")
+@Table(name = "testimonials")
+@EntityListeners(AuditingEntityListener.class)
+
+public class TestimonialEntity {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @NotNull
+    private String name;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(columnDefinition = "timestamp")
+    private Timestamp timestamp;
+    private boolean softDelete;
+}
