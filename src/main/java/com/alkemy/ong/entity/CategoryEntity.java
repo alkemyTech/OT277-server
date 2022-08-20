f59dfe1d6477647b9a54411a2ec5a49eaf2625fa
Juan Carlos Giordano <juancadeveloper@mac-mini-de-juan.local>
2022-08-20 11:20:04 -0300

    OT277-19 CategoryEntity and CategoryRepository added

---
+++ src/main/java/com/alkemy/ong/entity/CategoryEntity.java
@@ -0,0 +1,48 @@
+package com.alkemy.ong.entity;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+@SQLDelete(sql = "UPDATE categories SET softDelete = true WHERE id=?")
+@Where(clause = "softDelete=false")
+public class CategoryEntity {
+
+@Id
+@GeneratedValue(generator = "uuid")
+@GenericGenerator(name = "uuid", strategy = "uuid2")
+private String id;
+
+@Column(nullable = false)
+private String name;
+
+@Column(nullable = false)
+private String description;
+
+@Column(nullable = false)
+private String image;
+
+@CreationTimestamp
+private Timestamp timestamps;
+
+private Boolean softDelete = Boolean.FALSE;
+
+}
