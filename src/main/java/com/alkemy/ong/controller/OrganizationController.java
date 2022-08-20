253764c4da6e48ba849d0a32314c43861de7bd40
Lautaro <lautafuriasse16@gmail.com>
2022-08-19 23:58:01 -0300

    OT277-24 Added and endpoint for public information

---
+++ src/main/java/com/alkemy/ong/controller/OrganizationController.java
@@ -0,0 +1,24 @@
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.dto.SimpleOrganizationDTO;
+import com.alkemy.ong.service.OrganizationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/organization")
+@RequiredArgsConstructor
+public class OrganizationController {
+
+    private final OrganizationService organizationService;
+
+    @GetMapping("/public")
+    public ResponseEntity<?> publicInformation(){
+        SimpleOrganizationDTO result = organizationService.getPublicInformation();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
