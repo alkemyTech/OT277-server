cd5ccd3d385b6ec44b9775e9073f175d347780d3
Luciano Toledo <toledolucianogabriel@gmail.com>
2022-08-20 16:16:49 -0300

    Added annotations to UserEntity and UserRepository created

--- src/main/java/com/alkemy/ong/entity/UserEntity.java
+++ src/main/java/com/alkemy/ong/entity/UserEntity.java
@@ -4,50 +4,52 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
-@Table(name = "USERS")
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "UPDATE organizations SET soft_delete = true WHERE id = ?")
+@Where(clause = "soft_delete = false")
+@Table(name = "users", indexes = @Index(name = "idx_users_email", columnList = "email"))
 public class UserEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "USER_ID", nullable = false, unique = true)
-    private Long userId;
 
-    @Column(name = "FIRST_NAME", nullable = false)
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "LAST_NAME", nullable = false)
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "EMAIL", nullable = false, unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(name = "PASSWORD", nullable = false)
+    @Column(nullable = false)
     private String password;
 
-    @Column(name = "PHOTO")
     private String photo;
+
+    @CreatedDate
+    @Column(nullable = false, columnDefinition = "timestamp")
+    private Timestamp timestamps;
+
+    @Column(name = "soft_delete")
+    private boolean softDelete;
 
 //    @Column(name = "ROLE_ID", nullable = false)
 //    @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
 //    private List<Role> roles;
-
-    @Column(name = "TIMESTAMP", nullable = false)
-    @CreationTimestamp
-    private Timestamp timestamps;
-
-    @Column(name = "SOFT_DELETE")
-    private boolean softDelete;
 }
