2a51b1a4c93e24b2ad7927dd448a6b8c42b64e4d
Lautaro <lautafuriasse16@gmail.com>
2022-08-20 00:01:45 -0300

    OT277-24 Added mapper function

---
+++ src/main/java/com/alkemy/ong/mapper/imp/OrganizationMapper.java
@@ -0,0 +1,19 @@
+package com.alkemy.ong.mapper.imp;
+
+import com.alkemy.ong.dto.SimpleOrganizationDTO;
+import com.alkemy.ong.entity.OrganizationEntity;
+import com.alkemy.ong.mapper.Mapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrganizationMapper implements Mapper<SimpleOrganizationDTO, OrganizationEntity> {
+    @Override
+    public SimpleOrganizationDTO toDto(OrganizationEntity organizationEntity) {
+        var dto = new SimpleOrganizationDTO();
+        dto.setName(organizationEntity.getName());
+        dto.setAddress(organizationEntity.getAddress());
+        dto.setImage(organizationEntity.getImage());
+        dto.setPhone(organizationEntity.getPhone());
+        return dto;
+    }
+}
