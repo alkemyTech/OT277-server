c143b8f9cb4d62f0e003815dbe722d828c463f48
LautaLFS <lautafuriasse16@gmail.com>
2022-08-21 21:06:54 -0300

    OT277-24 Fix

--- src/main/java/com/alkemy/ong/mapper/Mapper.java
+++ src/main/java/com/alkemy/ong/mapper/Mapper.java
@@ -2,11 +2,7 @@ package com.alkemy.ong.mapper;
 
 import org.springframework.lang.NonNull;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public interface Mapper<D, E> {
-
     D toDto(@NonNull E e);
 
 }
