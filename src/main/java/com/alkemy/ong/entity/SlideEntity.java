7bb22bcdce3057a89ff19333a06ec6307e80b255
spateranahuel <spateranahuel@gmail.com>
2022-08-22 17:27:30 -0300

    OT277-29 Slide: Respository and DB model

---
+++ src/main/java/com/alkemy/ong/entity/SlideEntity.java
@@ -0,0 +1,35 @@
+package com.alkemy.ong.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "slides")
+public class SlideEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String imageUrl;
+
+    @Column(columnDefinition = "text")
+    private String text;
+
+    private String order;
+
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "organizationId",insertable = false,updatable = false)
+    private OrganizationEntity organizationEntity;
+
+    @Column(name = "organizationId",nullable = false)
+    private String organizationId;
+
+}
