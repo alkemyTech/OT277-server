7e8cc3ee8b3a31ddd4962d019dacd3c68110b569
Ezequiel Lutter <lutterezequiel@gmail.com>
2022-08-23 00:23:25 -0300

    OT277-22/23

---
+++ src/main/java/com/alkemy/ong/entity/ActivityEntity.java
@@ -0,0 +1,47 @@
+package com.alkemy.ong.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "activities", indexes = @Index(name = "idx_activities_name", columnList = "activity"))
+public class ActivityEntity {
+
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name="uuid", strategy = "uuid2")
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false, columnDefinition = "timestamp")
+    private Timestamp timestamp;
+
+    @Column(name = "soft_delete")
+    private boolean softDelete;
+
+
+
+
+
+
+}
---
+++ src/main/java/com/alkemy/ong/entity/MemberEntity.java
@@ -0,0 +1,55 @@
+package com.alkemy.ong.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "members", indexes = @Index(name = "idx_members_name", columnList = "member"))
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name="uuid", strategy = "uuid2")
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "facebook_url", nullable = false)
+    private String facebookUrl;
+
+    @Column(name ="instagram_url", nullable = false)
+    private String instagramUrl;
+
+    @Column(name = "linkedin_url", nullable = false)
+    private String linkedinUrl;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(columnDefinition = "timestamp")
+    @CreatedDate
+    private Timestamp timestamp;
+
+    @Column(name = "soft_delete")
+    private boolean softDelete;
+
+
+}
