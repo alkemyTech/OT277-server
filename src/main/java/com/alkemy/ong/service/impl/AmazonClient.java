8ddaff3d564497a2e6b5564bf40abe1e3d6ee348
LautaLFS <lautafuriasse16@gmail.com>
2022-08-26 13:26:12 -0300

    Hotfix

--- src/main/java/com/alkemy/ong/service/impl/AmazonClient.java
+++ src/main/java/com/alkemy/ong/service/impl/AmazonClient.java
@@ -5,7 +5,6 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
