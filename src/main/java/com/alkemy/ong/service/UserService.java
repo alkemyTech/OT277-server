4a113d50ac1a44eafaa7616f1fe4f5820521a52c
LautaLFS <lautafuriasse16@gmail.com>
2022-09-05 13:15:51 -0300

    Ticket 36 finished

--- src/main/java/com/alkemy/ong/service/UserService.java
+++ src/main/java/com/alkemy/ong/service/UserService.java
@@ -1,10 +1,15 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.dto.UserDto;
 import com.alkemy.ong.entity.UserEntity;
+
+import java.util.List;
 
 public interface UserService {
 
     UserEntity getByEmail(String email);
 
     void validateEmail(String email);
+
+    List<UserDto> findAll();
 }
