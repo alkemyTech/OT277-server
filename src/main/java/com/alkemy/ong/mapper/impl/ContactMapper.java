047582df837c13154fc23779baca77fb33d08a68
Cristian Monzon <cristianmonzon91@gmail.com>
2022-09-12 22:13:15 -0300

    OT277-62 checkeado y finalizado

--- src/main/java/com/alkemy/ong/mapper/impl/ContactMapper.java
+++ src/main/java/com/alkemy/ong/mapper/impl/ContactMapper.java
@@ -1,9 +1,13 @@
 package com.alkemy.ong.mapper.impl;
 
 import com.alkemy.ong.dto.ContactDTO;
+import com.alkemy.ong.entity.CategoryEntity;
 import com.alkemy.ong.entity.ContactEntity;
 import com.alkemy.ong.mapper.Mapper;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ContactMapper implements Mapper<ContactDTO, ContactEntity> {
@@ -32,5 +36,15 @@ public class ContactMapper implements Mapper<ContactDTO, ContactEntity> {
     public ContactDTO toDto(ContactEntity entity) {
 
         return null;
+    }
+
+    public List<ContactDTO> contactEntityList2DTOList(List<ContactEntity> ContactEntitiesList){
+
+        List<ContactDTO> Contacts = new ArrayList<>();
+
+        for (ContactEntity entity: ContactEntitiesList){
+            Contacts.add(toBasicDto(entity));
+        }
+        return Contacts;
     }
 }
