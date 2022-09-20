a95a5472b198e297fb1dac5e27fb78d92f271687
LautaLFS <lautafuriasse16@gmail.com>
2022-09-20 19:03:58 -0300

    OT277-87

--- src/main/java/com/alkemy/ong/dto/CategoryDTO.java
+++ src/main/java/com/alkemy/ong/dto/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,12 +11,16 @@ import java.sql.Timestamp;
 @Setter
 public class CategoryDTO {
 
+    @ApiModelProperty(example = "Tech", position = 1)
     @NotBlank(message = "Name cannot be empty or null.")
     private String name;
 
+    @ApiModelProperty(example = "Tech's category", position = 2)
     private String description;
 
+    @ApiModelProperty(example = "https://cohorte-agosto-38d749a7.s3.amazonaws.com/picture.png", position = 3)
     private String image;
 
+    @ApiModelProperty(dataType = "Date", example = "2022/09/20", position = 4)
     private Timestamp timestamps;
 }
--- src/main/java/com/alkemy/ong/dto/NewDtoResponse.java
+++ src/main/java/com/alkemy/ong/dto/NewDtoResponse.java
@@ -1,5 +1,7 @@
 package com.alkemy.ong.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,20 +14,28 @@ import java.util.Set;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class NewDtoResponse {
 
+    @ApiModelProperty(example = "Juan", position = 2, required = true)
     private String name;
 
+    @ApiModelProperty(example = "Content of the post", position = 3, required = true)
     private String content;
 
+    @ApiModelProperty(example = "https://cohorte-agosto-38d749a7.s3.amazonaws.com/picture.png", position = 5)
     private String image;
 
+    @ApiModelProperty(required = true)
     private CategoryDTO category;
 
+    @ApiModelProperty(dataType = "Date", example = "2022/09/20", position = 4)
     private Timestamp timestamps;
 
+    @ApiModelProperty(example = "News", position = 1)
     private String type;
 
+    @ApiModelProperty(example = "comments", position = 6)
     Set<CommentDtoResponse> comments = new HashSet<>();
 
 }
