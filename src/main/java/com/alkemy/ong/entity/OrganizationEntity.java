83686716f3f47ee2afa05f42917634b4a4f6063a
LucianoToledo <toledolucianogabriel@gmail.com>
2022-09-20 00:01:04 -0300

    OT277-90 remove unused import

--- src/main/java/com/alkemy/ong/entity/OrganizationEntity.java
+++ src/main/java/com/alkemy/ong/entity/OrganizationEntity.java
@@ -12,7 +12,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
